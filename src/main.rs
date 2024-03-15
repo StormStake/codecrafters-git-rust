fa913218efc543c27ac23c3060c59b25710376ea
StormStake <59213963+StormStake@users.noreply.github.com>
2024-03-14 19:58:31 -0500

    First stage

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,43 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use clap::Subcommand;
+use clap::Parser;
+
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: Option<String>,
+
+    #[command(subcommand)]
+    command: Command
+}
+
+#[derive(Debug, Subcommand)]
+enum Command
+{
+    Init {
+        
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
+    let args = Args::parse();
+    match args.command {
+        Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+    }
     // Uncomment this block to pass the first stage
     // let args: Vec<String> = env::args().collect();
     // if args[1] == "init" {
